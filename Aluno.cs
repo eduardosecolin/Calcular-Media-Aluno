07c62e58ddfe19d6ba3e37f712080480e2eda824
Carlos Eduardo Secolin <30784184+eduardosecolin@users.noreply.github.com>
2017-09-07 22:22:39 -0300

    aluno
    facebook.com/eduardosecolinart

---
+++ Aluno.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    class Aluno
+    {
+        //atributos
+        private string nome;
+        private string curso;
+        private double nota1;
+        private double nota2;
+        private double media;
+        //metodos especiais
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+        public string getCurso()
+        {
+            return this.curso;
+        }
+        public void setCurso(string curso)
+        {
+            this.curso = curso;
+        }
+        public double getNota1()
+        {
+            return this.nota1;
+        }
+        public void setNota1(double nota1)
+        {
+            this.nota1 = nota1;
+        }
+        public double getNota2()
+        {
+            return this.nota2;
+        }
+        public void setNota2(double nota2)
+        {
+            this.nota2 = nota2;
+        }
+        public double getMedia()
+        {
+            return this.media;
+        }
+        public void setMedia(double media)
+        {
+            this.media = media;
+        }
+        //metodos
+        public string imprimir()
+        {
+            string texto = "O nome: " + this.getNome() + "O curso: " + this.getCurso() + "A media: " + this.getMedia();
+            return texto;
+        }
+        public double calcularMedia()
+        {
+            media = (this.getNota1() + this.getNota2()) / 2;
+            return this.media;
+        }
+        public bool aprovado()
+        {
+            if(this.getMedia() >= 7)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercicio1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Aluno a = new Aluno();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCarregar_Click(object sender, RoutedEventArgs e)
+        {
+            a.setNome(txtNome.Text);
+            a.setCurso(txtCurso.Text);
+            a.setNota1(double.Parse(txtNota1.Text));
+            a.setNota2(double.Parse(txtNota2.Text));
+            
+        }
+
+        private void btnImprimir_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(a.imprimir());
+        }
+
+        private void btnLimpar_Click(object sender, RoutedEventArgs e)
+        {
+            txtNome.Clear();
+            txtCurso.Clear();
+            txtNota1.Clear();
+            txtNota2.Clear();
+            txtMedia.Clear();
+        }
+
+        private void btnCalcular_Click(object sender, RoutedEventArgs e)
+        {
+            a.calcularMedia();
+            txtMedia.Text = Convert.ToString(a.getMedia());
+        }
+
+        private void btnAprovado_Click(object sender, RoutedEventArgs e)
+        {
+            if(a.aprovado())
+            {
+                MessageBox.Show("Aprovado!");
+            }
+            else
+            {
+                MessageBox.Show("Reprovado!");
+            }
+        }
+    }
+}
